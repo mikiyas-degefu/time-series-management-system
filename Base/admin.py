209cc61b6dac058b1e7e1c1078a9e28655d26202
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-09-05 16:18:44 +0300

    data added

--- Base/admin.py
+++ Base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 from .resource import(
     TopicResource,
+    CategoryResource
 )
 
 
@@ -11,4 +12,10 @@ class TopicAdmin(ImportExportModelAdmin):
     resource_classes = [TopicResource]
 
 admin.site.register(Topic, TopicAdmin)
+
+
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_classes = [CategoryResource]
+
+admin.site.register(Category, CategoryAdmin)
 
--- Base/resource.py
+++ Base/resource.py
@@ -1,8 +1,38 @@
-from import_export import resources
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget 
 from .models import (
-    Topic
+    Topic,
+    Category,
 )
 class TopicResource(resources.ModelResource):
 
     class Meta:
         model = Topic
+        skip_unchanged = True
+        report_skipped = True
+        exclude = ( 'id', 'updated', 'created', 'is_deleted', 'rank', 'icon', 'is_dashboard')
+        import_id_fields = ('title_ENG', 'title_AMH')
+
+
+class CategoryResource(resources.ModelResource):
+    topic = fields.Field(
+        column_name='topic',
+        attribute='topic',
+        widget=ForeignKeyWidget(Topic, field='title_ENG'),
+        saves_null_values = True,
+        )
+
+
+    def before_import_row(self, row, **kwargs):
+            if row.get('topic') is None:
+               pass
+            else :
+               topic_name = row["topic"]
+               Topic.objects.get_or_create(title_ENG=topic_name, defaults={"title_ENG": topic_name, "title_AMH":topic_name})
+    
+    class Meta:
+        model = Category
+        report_skipped = True
+        skip_unchanged = True
+        exclude = ( 'id', 'created_at', 'is_deleted','is_dashboard_visible', )
+        import_id_fields = ('name_ENG', 'name_AMH','topic')
