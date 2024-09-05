ac7cef11d465105048e27431b57bfa0e55fdeea9
Kaleab <benjiyg400@gmail.com>
2024-09-05 10:56:34 +0300

    adminregister

--- Base/admin.py
+++ Base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from.models import *
 # Register your models here.
+
+
+admin.site.register(Topic)
+admin.site.register(Document)
+admin.site.register(Category)
+admin.site.register(Indicator)
+
+admin.site.register(DataPoint)
+admin.site.register(Indicator_Point)
+admin.site.register(Quarter)
+
+admin.site.register(Month)
+admin.site.register(QuarterData)
+admin.site.register(MonthData)
+
+admin.site.register(AnnualData)
+admin.site.register(Source)
--- Base/models.py
+++ Base/models.py
@@ -175,6 +175,7 @@ class Source(models.Model):
     updated =  models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default = False)
+    
     def __str__(self):
         return self.title_ENG
 
