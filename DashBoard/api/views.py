747fefd60593d181c940d59265e09bc79b03594f
Kaleab <benjiyg400@gmail.com>
2024-10-21 16:15:53 +0300

    ------

--- DashBoard/api/views.py
+++ DashBoard/api/views.py
@@ -2,16 +2,18 @@ from DashBoard.serializer import(
     ComponentSerializer, 
     DashboardIndicatorSerializer,
     AnnualDataSerializers
-    
     )
 
+from django.http import HttpResponse     
+
 from Base.models import (
-    AnnualData
+    AnnualData , Indicator
 )
 from DashBoard.models import Component, DashboardIndicator
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import status
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def components(request):
@@ -29,3 +31,21 @@ def components(request):
         }
         return Response(context)
 
+
+
+
+
+@api_view(['GET'])
+def single_indicator_data(request, id, year):
+    if request.method == 'GET':
+        try:
+            indicator = Indicator.objects.get(id=id)
+            annual_data = AnnualData.objects.get(indicator=indicator, for_datapoint__year_EC=year)
+
+            serializer = AnnualDataSerializer(annual_data)  # Serialize the annual_data object
+
+            return Response(serializer.data)
+        except Indicator.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except AnnualData.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- DashBoard/urls.py
+++ DashBoard/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 
     ###Api
     path('components/', api.components),
+    path('single_indicator_data/<str:id>/<str:year>/', api.single_indicator_data),
 ]
